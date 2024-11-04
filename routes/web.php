c17597a879392e6a572a5692aa949c3ba6424a12
JeromeMaligaya <jeromehapi_97@yahoo.com>
2024-11-04 23:53:06 +0100

    added routes for AdminProjectController

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\Admin\ProjectController as AdminProjectController;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +22,10 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::middleware("auth")->prefix("/admin")->name("admin.")->group(function () {
+    Route::get("/projects", [AdminProjectController::class, "index"])->name("projects.index");
+    Route::get("/projects/{id}", [AdminProjectController::class, "show"])->name("projects.show");
+    Route::get("/projects/create", [AdminProjectController::class, "create"])->name("projects.create");
+    Route::get("/projects", [AdminProjectController::class, "store"])->name("projects.store");
+});
