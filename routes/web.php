66b93bd876f646f50b0a7cb896f61357416eeebd
JeromeMaligaya <jeromehapi_97@yahoo.com>
2024-11-09 00:48:34 +0100

    added routes for deletedIndex, destroy, permanentDelete and restore on admin/projects in web.php

--- routes/web.php
+++ routes/web.php
@@ -25,9 +25,13 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 
 Route::middleware("auth")->prefix("/admin")->name("admin.")->group(function () {
     Route::get("/projects", [AdminProjectController::class, "index"])->name("projects.index");
+    Route::get('/projects/deleted', [AdminProjectController::class, "deletedIndex"])->name("projects.deleted.index");
     Route::get("/projects/create", [AdminProjectController::class, "create"])->name("projects.create");
-    Route::get("/projects/{id}", [AdminProjectController::class, "show"])->name("projects.show");
+    Route::get("/projects/{project}", [AdminProjectController::class, "show"])->name("projects.show");
     Route::post("/projects", [AdminProjectController::class, "store"])->name("projects.store");
-    Route::get('/projects/{id}/edit', [AdminProjectController::class, "edit"])->name("projects.edit");
-    Route::put('/projects/{id}', [AdminProjectController::class, "update"])->name("projects.update");
+    Route::get('/projects/{project}/edit', [AdminProjectController::class, "edit"])->name("projects.edit");
+    Route::put('/projects/{project}', [AdminProjectController::class, "update"])->name("projects.update");
+    Route::delete('projects/{id}', [AdminProjectController::class, "destroy"])->name("projects.delete");
+    Route::delete('projects/{id}/permanent-delete', [AdminProjectController::class, "permanentDelete"])->name("projects.permanent.delete");
+    Route::patch('/projects/{id}/restore', [AdminProjectController::class, "restore"])->name("projects.restore");
 });
